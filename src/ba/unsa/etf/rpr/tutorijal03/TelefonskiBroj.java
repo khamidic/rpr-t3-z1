3d18c006399774462cddbe0eceff260dde027c53
Kenan Hamidic <kenanhamidic@gmail.com>
2018-10-31 23:23:57 +0100

    Apstratkna klasa "TelefonskiBroj" iplementirana

---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public abstract class TelefonskiBroj {
+    private String telefonskiBroj;
+
+    public abstract void Ispisi();
+    @Override
+    public abstract int hashCode();
+}
